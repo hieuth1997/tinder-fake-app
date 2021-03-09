e44a9385fde637fdc76ac71c5d8c7282d3f2999d
Hieu Nguyen Trong <hieuth1997@gmail.com>
2021-03-09 09:32:39 +0700

    create tinder card

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import TinderCard from './Components/TinderCard';
 function App() {
   return (
     <div>
       <h1>let build the tinder clone app</h1>
+      <TinderCard></TinderCard>
     </div>
   );
 }
